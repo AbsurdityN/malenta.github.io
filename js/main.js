559a841f65409c254c92456bcb23a2536c3638c3
AbsurdityN <139918709+AbsurdityN@users.noreply.github.com>
2024-01-07 22:29:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=slovakia&appid=21f51af4daba9eecb099b8e03a553187";
+const API_KEY = "21f51af4daba9eecb099b8e03a553187";
 
+fetch(WEATHER_URL) 
+.then(response => response.json())
+.then(responseJson => {
+    document.querySelector("name").innerHTML = responseJson.name
+})
